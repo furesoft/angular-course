93af060f607fb989177733772ec44229ddc98870
Chris Anders <chris.anders@schwaebisch-hall.de>
2024-03-08 09:54:08 +0100

    Cleanup

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from "@angular/core";
 import { AuthComponent } from "./auth.component";
 import { SharedModule } from "../shared/shared.module";
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+    { path: "auth", component: AuthComponent }
+];
 
 @NgModule({
     declarations: [
         AuthComponent,
     ],
     imports: [
-        SharedModule
+        SharedModule,
+        RouterModule.forChild(routes)
     ]
 })
 export class AuthModule {
